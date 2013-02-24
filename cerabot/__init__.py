cdd69acea486716aa3e3f45bccd96a23b7fb51b8
ceradon <ceradon_wiki@hotmail.com>
2013-02-23 20:40:50 -0500

    Update cerabot/__init__.py

--- cerabot/__init__.py
+++ cerabot/__init__.py
@@ -1,1 +1,6 @@
-from wikitools import *
+import wikitools.api as api
+import wikitools.wiki as wiki
+import wikitools.page as page
+import wikitools.user as user
+import wikitools.wikifile as file
+import wikitools.category as category
