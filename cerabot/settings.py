e08a9d78ea89b07f9b13ce1d056b3920e6bc8ec7
ceradon <ceradon_wiki@hotmail.com>
2013-02-24 17:25:19 -0500

    Update, in accordance with PEP8

--- cerabot/settings.py
+++ cerabot/settings.py
@@ -13,7 +13,8 @@ class Settings(object):
 
         #Wikipedia settings
         self.settings['run_base'] = u"User:Cerabot/Run/Task {task}"
-        self.settings['summary_end'] = u". ([[User:Cerabot/Run/Task {task}|bot]])"
+        self.settings['summary_end'] = u". ([[User:Cerabot/Run/Task "+
+                                                    "{task}|bot]])"
 
     @property
     def settings(self):
