2c55d0e8f494b1326e29e8cfafb4f4d672e4c784
ceradon <ceradon_wiki@hotmail.com>
2013-02-24 23:46:32 -0500

    Update.

--- cerabot/bot.py
+++ cerabot/bot.py
@@ -1,4 +1,5 @@
 import sys
+import os.path as path
 from .. import cerabot
 
 class Bot(object):
@@ -8,12 +9,15 @@ class Bot(object):
 
     def __init__(self):
         """Initiates the Bot class."""
+        #Builds several important variales.
         self.is_logged_in = False
         self.site_api = None
         self.wiki = None
         self.settings = settings.Settings().settings
         if not self.settings:
-            raise MissingSettingsError("Variable `settings` is empty")
+            raise exceptions.MissingSettingsError("Variable `settings`"
+                                                  "is empty")
+        self.summary = self.settings['summary']
 
         #Assemble bot variables
         self.user = self.settings['user']
@@ -21,13 +25,17 @@ class Bot(object):
             self.passwd = self.settings['passwd']
         elif not self.settings['passwd'] and
                 self.settings['passwd_file']:
-            file = open(self.settings['passwd_file'], 'r')
-            self.passwd = file.read()
-            file.close()
+            if path.isfile(self.settings['passwd_file']):
+                file = open(self.settings['passwd_file'], 'r')
+                self.passwd = file.read()
+                file.close()
+            else:
+                raise exceptions.NoPasswordFileError("`passwd`"+
+                        "does not exist")
         elif not self.settings['passwd'] and 
                 not self.settings['passwd_file']:
-            raise NoPasswordError("Variables `passwd` and "+
-                    "`passwd_file` were not defined")
+            raise exceptions.NoPasswordError("Variables `passwd` "+
+                    "and `passwd_file` were not defined")
 
         #Build site API URL
         self.site_api = self._build_site_api(
@@ -36,6 +44,47 @@ class Bot(object):
         
         #Login to site API
         self.wiki.login(self.user, self.passwd)
+        self.setup()
+
+    def setup(self):
+        """Called immediately after Bot is initiated.
+        Idles by default.
+        """
+        pass
+
+    def run(self):
+        """Main method by which the task does stuff and also
+        serves as the main entry point for the task.
+        """
+        pass
+
+    def run_page_enabled(self):
+        """Checks if the run page for
+        Cerabot is enabled.
+        """
+        run_page = self.settings['run_base']
+        run_page = run_page.format(task=self.task)
+        page_ = page.Page(self.wiki, run_page)
+        text = page_.getWikiText()
+        if not text == 'yes':
+            raise exceptions.RunPageDisabledError("Run page is disabled.")
+        else:
+            return True
+
+    def build_summary(self, comment):
+        """Builds the summary for every edit 
+        the task will make. Param `comment` is
+        an optional message. Defaults to 
+        'Automated edit by [[User:Cerabot]]'
+        """
+        default = "Automated edit by [[User:Cerabot]]"
+        if not comment:
+            self.summary.format(task=unicode(self.task,
+                                comment=default)
+        else:
+            self.summary.format(task=unicode(self.task,
+                                comment=comment)
+        return self.summary
 
     def _build_site_api(self, args):
         """Builds the site's api URL
@@ -51,15 +100,12 @@ class Bot(object):
             template = template.format(site=args[0], lang=args[1])
         return
 
-    def run_page_enabled(self):
-        """Checks if the run page for
-        Cerabot is enabled.
-        """
-        run_page = self.settings['run_base']
-        run_page = run_page.format(task=self.task)
-        page_ = page.Page(self.wiki, run_page)
-        text = page_.getWikiText()
-        if not text == 'yes':
-            raise RunPageDisabledError("Run page is disabled.")
-        else:
-            return True
+    def __repr__(self):
+        """Return the canonical string representation of the Task."""
+        res = "Bot(name={0!r}, task={1!r})"
+        return res.format(self.name, self.task)
+
+    def __str__(self):
+        """Return a prettier string representation of the Task."""
+        res = "<Bot {0} (Task {1})>"
+        return res.format(self.name, self.task)
