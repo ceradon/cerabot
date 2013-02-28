09f9b3218e98fb65a7d20c8cfb861ca1f2a06890
ceradon <ceradon_wiki@hotmail.com>
2013-02-27 22:45:22 -0500

    Start task code.

--- cerabot/tasks/date_templates.py
+++ cerabot/tasks/date_templates.py
@@ -1,1 +1,8 @@
+import sys
 from cerabot import *
+
+class DateTemplates(bot.Bot):
+    """Bot to date maintenance templates that have none."""
+    def __init__(self):
+        #Initiates the `super` function
+        super(DateTemplates, self)__init__()
