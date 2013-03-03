8599b95d1721c12a0539fe5fe2acb442f502c075
ceradon <ceradon_wiki@hotmail.com>
2013-03-03 16:51:53 -0500

    Added several frontend and other functions.

--- cerabot/irc/connector.py
+++ cerabot/irc/connector.py
@@ -16,6 +16,8 @@ class Connection(object):
         self._last_pang = 0
         self._last_recvd = time.time()
         
+        self._channels = []
+
     def connect(self):
         """Connect to the specified IRC server."""
         self.socket = socket.socket(socket.AF_INET, 
@@ -71,6 +73,11 @@ class Connection(object):
         if line[0] == "PING":
             self.send_data("PONG {0}".format(line[1][1:])
 
+    def _process_line(self):
+        """Processes a line from the server.
+        To be implemented in subclasses."""
+        raise NotImplementedError()
+
     def quit(self, msg=None):
         """Quits from the server, but doesn't close connection."""
         if msg:
@@ -93,4 +100,82 @@ class Connection(object):
                 line = line.strip().split()
                 self._process_pong(line)
 
+        self._close_conn()
+
+    def stayin_alive(self):
+        """'Feel the city breakin' and everybody shakin''
+        Keeps the connection alive, and ensures we are 
+        still connected to the server."""
+        now = time.time()
+        if now - self._last_recvd > 120:
+            if self._last_ping < self._last_recvd:
+                print "The servers been still for awhile; Pinging..."
+                self.ping(self.host)
+            elif now - self._last_ping > 60:
+                print "Haven't heard back from the server;"+ \
+                        "Shutting down..."
+                self._shutdown("Ba-bye.")
+
+    def _split_message(msgs, maxlen, maxsplits=5):
+        """Split a large message into multiple messages smaller than maxlen.
+        Credit goes to Ben Kurtovic (Earwig) for writing this awesome piece
+        of code."""
+        words = msgs.split(" ")
+        splits = 0
+        while words and splits < maxsplits:
+            splits += 1
+            if len(words[0]) > maxlen:
+                word = words.pop(0)
+                yield word[:maxlen]
+                words.insert(0, word[maxlen:])
+            else:
+                msg = []
+                while words and len(" ".join(msg + [words[0]])) <= maxlen:
+                    msg.append(words.pop(0))
+                yield " ".join(msg)
+
+    def join(self, chan):
+        """Joins a channel on the server."""
+        self._send_data("JOIN {0}".format(chan)
+        self._channels.append(chan)
+
+    def part(self, chan, msg=None):
+        """Parts a channel on the server."""
+        if msg:
+            self._send_data("PART {0} :{1}".format(chan, msg))
+        else:
+            self._send_data("PART {0}".format(chan))
+        try:
+            self._channels.remove(chan)
+        except ValueError:
+            #We're not in that channel.
+            pass
+
+    def say(self, msg, target):
+        """Sends PRIVMSG `msg` to `target`."""
+        for line in self._split_message(msg, 220):
+            self._send_data("PRIVMSG {0} :{1}".format(target,
+                                                      msg))
+
+    def action(self, msg):
+        """Sends ACTION `msg` to server."""
+        self._send_data("\x01ACTION {0}\x01".format(msg))
+
+    def mode(self, target, level, msg):
+        """Sends MODE `msg` to server."""
+        self._send_data("MODE {0} {1} {2}".format(target, level
+                                                  msg))
+
+    def notice(self, target, msg):
+        for line in self._split_message(msg, 220):
+            self._send_data("NOTICE {0} :{1}".format(target, 
+                                                     msg)
+
+    def ping(self, target):
+        """Sends a PING message to `target`."""
+        self._send_data("PING {0}".format(target))
+        self._last_ping = time.time()
+
+    def shutdown(self, msg):
+        self.quit(msg)
         self._close_conn()
