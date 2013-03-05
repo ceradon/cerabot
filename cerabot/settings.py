c38e29a672dbe463466558c60840d7b04c3fad6c
ceradon <ceradon_wiki@hotmail.com>
2013-03-04 21:50:26 -0500

    Update settings.py

--- cerabot/settings.py
+++ cerabot/settings.py
@@ -15,3 +15,11 @@ class Settings(object):
         self.settings['run_base'] = u"User:Cerabot/Run/Task {task}"
         self.settings['summary'] = u"Task {task}: {comment}. "
         self.settings['summary'] += "([[User:Cerabot/Run/Task {task}|bot]])"
+        
+        #IRC settings
+        self.settings['irc_nick'] = u"Cerabot"
+        self.settings['irc_passwd'] = u""
+        self.settings['irc_server'] = u"irc.freenode.net", 6667
+        self.settings['irc_name'] = u"IRC extension to Wikipedia robot Cerabot."
+        self.settings['irc_ident'] = u"cerabot"
+        self.settings['join_on_startup'] = ["##cerabot", "##ceradon"]
