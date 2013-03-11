cf66483c1c062c14837bca865ed0830b66f6b718
ceradon <ceradon_wiki@hotmail.com>
2013-03-11 20:26:36 -0300

    Update parser.py

--- cerabot/irc/parser.py
+++ cerabot/irc/parser.py
@@ -40,7 +40,7 @@ class Parser(connection.Connection):
         if self._line[0] == "PING":
             return False
         self.nick, self.ident, self.host = re.findall(r":(.*?)!(.*?)@(.*?)\Z",
-                self._line[0])
+                self._line[0])[0]
         self.chan = self._line[2]
         self.msg_type = self._line[1]
         self.msg = " ".join(self._line[3:])
