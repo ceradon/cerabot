519794a260840380f4c0efd7bff04b41b918744c
ceradon <ceradon_wiki@hotmail.com>
2013-03-12 18:47:13 -0300

    Update watcher.py

--- cerabot/irc/watcher.py
+++ cerabot/irc/watcher.py
@@ -9,11 +9,5 @@ class Watcher(irc.IRC):
     to parse the data from IRC."""
     def __init__(self):
         self.rc_parser = re.RC
-        self.re_color = re.compile("\x03[0-9]{1,2}(,[0-9]{1,2}?)?")
-        self.re_rc_edit = re.compile("\A\[\[(.*)?\]\]\s(.*)?\s?(\d+)?"+ \
-                "http://en.wikipedia.org/w/index.php\?diff=(.*)&oldid=(.*)"+ \
-                "\s+?\*\s(.*)\s\*\s(\(\+\d+\))?\s(.*)\Z")
-        self.re_rc_log = re.compile("\A\[\[(.*)?\]\]\s(\w+)?\s\*\s(.*)"+ \
-                "\s\*\s\s(.*)\Z")
         super(Watcher, self).__init__(rc_watch=True, _line_parser=
                                       self._parse_rc_line)
