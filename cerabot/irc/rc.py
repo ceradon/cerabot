003fe376ceb71e2783147445e39e23441dd0f1d9
ceradon <ceradon_wiki@hotmail.com>
2013-03-12 19:03:19 -0300

    Getting an sre_constants error, let's try this.

--- cerabot/irc/rc.py
+++ cerabot/irc/rc.py
@@ -14,7 +14,7 @@ class RC(object):
         self.re_color = re.compile("\x03[0-9]{1,2}(,[0-9]{1,2}?)?")
         self.re_rc_edit = re.compile("\A\[\[(.*)?\]\]\s(.*)?\s?(\d+)?"+ \
                 "http://en.wikipedia.org/w/index.php\?diff=(.*)&oldid=(.*)"+ \
-                "\s+?\*\s(.*)\s\*\s(\(\+\d+\))?\s(.*)\Z")
+                "\s+?\*\s(.*?)\s\*\s(\(\+\d+\))?\s(.*?)\Z")
         self.re_rc_log = re.compile("\A\[\[(.*)?\]\]\s(\w+)?\s\*\s(.*)"+ \
                 "\s\*\s\s(.*)\Z")
 
