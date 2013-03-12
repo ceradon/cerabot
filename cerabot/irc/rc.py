85837653e645bd55a36367f583d79df70b6beb23
ceradon <ceradon_wiki@hotmail.com>
2013-03-12 20:33:59 -0300

    Recoding of the RC object to maximize efficiency.

--- cerabot/irc/rc.py
+++ cerabot/irc/rc.py
@@ -8,45 +8,65 @@ class RC(object):
         self.flags = None
         self.oldid = None
         self.title = u""
+        self.parser = parser
         self.comment = u""
         self.msg_type = u""
+        self.diff_size = None
 
-        self.re_color = re.compile("\x03[0-9]{1,2}(,[0-9]{1,2}?)?")
-        self.re_rc_edit = re.compile("\A\[\[(.*)?\]\]\s(.*)?\s?(\d+)?"+ \
-                "http://en.wikipedia.org/w/index.php\?diff=(.*)&oldid=(.*)"+ \
-                "\s+?\*\s(.*?)\s\*\s(\(\+\d+\))?\s(.*?)\Z")
-        self.re_rc_log = re.compile("\A\[\[(.*)?\]\]\s(\w+)?\s\*\s(.*)"+ \
-                "\s\*\s\s(.*)\Z")
+        self.valid_flags = {"!":"patrolled",
+                            "N":"new",
+                            "M":"minor",
+                            "B":"bot",}
 
         self._load()
 
     def _load(self):
-        if self.re_rc_edit.search(parser.msg):
-            edit = self.re_color.sub("", parser.msg)
-            edit = self.re_rc_edit.search(edit)
-            self.msg_type = u"edit"
-            self.title = edit.group(0)
-            if edit.group(1):
-                self.flags = edit.group(1)
-            self.diff = edit.group(3)
-            self.oldid = edit.group(4)
-            self.user = edit.group(5)
-            self.comment = edit.group(7)
-            result = []
-            if self.flags:
-                result = result.append(self.msg_type, self.title, 
-                        self.flags, self.comment, self.diff, self.oldid, 
-                        self.user)
-            else:
-                result = result.append(self.msg_type, self.title, 
-                        self.diff, self.oldid, self.user, self.comment)
-            return result
+        line = self.parser.msg
+        self.title = line[line.index("[["):line.index("]]")][2:]
+        line = line.replace("[[{0}]]".format(self.title), "")
+        line = line.strip().split(" ")
+        if line[0].strip("02").startswith("http://"):
+            x = re.findall("(\d+)?http://en.wikipedia.org/w/index.php"+ \
+                    "?diff\=(.*?)&oldid\=(.*?)", line[0])
+            self.diff, self.oldid = x[1:]
+        elif line[0][0] in self.valid_flags.keys():
+            self.flags = line[0]
+            x = re.findall("(\d+)?http://en.wikipedia.org/w/index.php"+ \
+                    "?diff=(.*?)&oldid=(.*?)", line[1])
+            self.diff, self.oldid = x[1:]
+        x = [i for i, x in enumerate(line) if x == "*"]
+        self.user = line[x[0]:[1]]
+        self.diff_size = line[x[1]+1].strip("(").strip(")")
+        self.comment = " ".join(line[x[1]+2:])
 
-        elif self.re_rc_log.search(parser.msg):
-            log = self.re_color.sub("", parser.msg)
-            log = self.re_rc_log.search(log)
-            self.msg_type = u"log"
-            self.log_type = log.group(0).replace(
-                    "Special:Log/", ""), log.group(1)
-            self.log_type = self.log_type + log.group(2), log.group(3)
-            return self.msg_type, self.log_type
+    """Allow easier access to some of the message's
+    attributes."""
+    @property
+    def title(self):
+        return self.title
+
+    @property
+    def diff(self):
+        return self.diff
+
+    @property
+    def oldid(self):
+        return self.oldid
+
+    @property
+    def flags(self):
+        if self.flags:
+            return self.flags
+        else:
+            return None
+
+    @property
+    def user(self):
+        return self.user
+
+    @property
+    def size(self):
+        return self.diff_size
+
+    def comment(self):
+        return self.comment
