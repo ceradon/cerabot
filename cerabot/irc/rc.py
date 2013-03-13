c4663a79f30b3decd2bc9a1cbe114ec785800bbd
ceradon <ceradon_wiki@hotmail.com>
2013-03-12 21:04:37 -0300

    Add functionality to load log messages.

--- cerabot/irc/rc.py
+++ cerabot/irc/rc.py
@@ -7,20 +7,31 @@ class RC(object):
         self.user = None
         self.flags = None
         self.oldid = None
-        self.title = u""
+        self.title = None
         self.parser = parser
         self.comment = u""
         self.msg_type = u""
         self.diff_size = None
+
+        #For log messages.
+        self.target = u""
+        self.log_type = None
 
         self.valid_flags = {"!":"patrolled",
                             "N":"new",
                             "M":"minor",
                             "B":"bot",}
 
-        self._load()
+        try:
+            self._load_edit()
+        except Exception:
+            try:
+                self._load_log()
+            except Exception:
+                raise Exception("Something went wrong! We we're "+ \
+                        "unable to parse this line.")
 
-    def _load(self):
+    def _load_edit(self):
         line = self.parser.msg
         self.title = line[line.index("[["):line.index("]]")][2:]
         line = line.replace("[[{0}]]".format(self.title), "")
@@ -38,6 +49,15 @@ class RC(object):
         self.user = line[x[0]:[1]]
         self.diff_size = line[x[1]+1].strip("(").strip(")")
         self.comment = " ".join(line[x[1]+2:])
+        self.msg_type = u"edit"
+
+    def _load_log(self):
+        line = self.parser.msg
+        line = line.split(" ")
+        self.log_type = line[0].strip("[[Special:Log/").strip("]]"), line[1]
+        self.target = line[3]
+        self.comment = " ".join(line[6:])
+        self.msg_type = u"log"
 
     """Allow easier access to some of the message's
     attributes."""
