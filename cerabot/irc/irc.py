35c5a0edaa8f5ff8d029bb71592add2f1fa4b6d0
ceradon <ceradon_wiki@hotmail.com>
2013-03-19 18:54:42 -0300

    A bit of restructuring to suit new command processing structure.

--- cerabot/irc/irc.py
+++ cerabot/irc/irc.py
@@ -4,11 +4,12 @@ import collections
 import os.path as path
 from cerabot import settings
 from cerabot import exceptions
+from .manager import CommandManager
 from cerabot.irc import rc
 from cerabot.irc import parser
 from cerabot.irc import connection
 
-__all__ = ["IRC", "Command"]
+__all__ = ["IRC"]
 
 class IRC(connection.Connection):
     def __init__(self, rc_watch=False, _line_parser=None):
@@ -17,9 +18,8 @@ class IRC(connection.Connection):
         commands when called, imports all commands."""
         self.settings = settings.Settings().settings
         self._last_conn_check = 0
-        self._commands = {}
-        self._command_obj = None
-        self._command_hooks = {}
+        self.commands = None
+        self._manager = CommandManager()
         self._line_parser = _line_parser
         self._nick = self.settings['irc_nick']
         dir = path.isfile(path.join(path.dirname(__file__), 
@@ -49,13 +49,14 @@ class IRC(connection.Connection):
         elif not self.rc_watch:
             self._host = self.settings['irc_server'][0]
             self._port = self.settings['irc_server'][1]
-#           self._assemble_commands()
             super(IRC, self).__init__(self._nick, self._passwd,
                   self._host, self._port, self._real_name, self._ident)
+            self.assemble_commands()
         self.connect()
 
     def _load_conn(self):
-#       self._command_obj = Command(irc=self)
+        """loop() over all IRC data, calling _process_line() for
+        each line in IRC, and keeps the connection alive."""
         if self.is_running:
             self.loop()
 
@@ -64,19 +65,19 @@ class IRC(connection.Connection):
             self._last_conn_check = time.time()
             time.sleep(320)
 
-    def _assemble_commands(self):
-        commands = [cls for cls in self._command_obj.__inheritors__.items()]
-        for command in commands:
-            self._commands[command.command_name.lower()] = []
-            self._commands[command.command_name.lower()].append(
-                    command.help_docs)
-            self._commands_hooks[command.command_name] = command.callable_hooks
-            self._commands_hooks[command.command_name].insert(0, 
-                    command.command_name)
+    def assemble_commands(self):
+        """Assembles all the commands we'll be using, putting the
+        name of the command and each command's hooks into one 
+        dictionary."""
+        self.commands = {}
+
+        for name in self._manager.resources.keys():
+            docs = self.get_docs(name)
+            hooks = self.get_hooks(name)
+            self.commands.update({name:hooks.insert(0, name), docs})
 
     def _process_line(self, line):
-        """Processes a single line from IRC. Should be
-        overrided."""
+        """Processes a single line from IRC."""
         parse = parser.Parser(line, self._nick)
         result = parse._load()
         if not result:
@@ -88,45 +89,42 @@ class IRC(connection.Connection):
         
     def _process_data(self, line, parse):
         """Processes a single line of data when _line_parser
-        is no specified."""
-        if parse.is_command:
-            if parse.command_name in self._command_hooks[parse.command_name]:
-                command = self.get_command_instance(parse.command_name)
-                if len(parse.args) < command.req_args:
-                    self.say(u"<bold>{0}<normal>, You have not provided "+ \
-                            "enough arguments to {1}".format(parse.nick, 
-                            parse.command_name), parse.chan)
-                elif len(parse.args) > command.req_args:
-                    self.say(u"<bold>{0}<normal>, {1} arguments required "+ \
-                            "for {2}, {3} given".format(parse.nick,
-                            unicode(parse.req_args), len(parse.args)))
-                else:
-                    command.call(args=parse.args.insert(0, parse), 
-                            kwargs=parse.kwargs)
+        is not specified."""
+        if parse.is_command and parse.command_name in self.commands:
+            command = self._manager.resources[parse.command_name]
+            a = command.has_args
+            if a[0]:
+                self.manager.call(parse.command_name)
             else:
-                return
+                if a[1] == "insufficient":
+                    e = u"<bold>{0}<normal>, You have not provided "+ \
+                            "enough arguments to {1}."
+                    self.reply(e.format(parse.nick, parse.command_name),
+                            parse)
+                elif a[1] == "excess":
+                    e = u"<bold>{0}<normal>, {1} arguments required "+ \
+                            "for {2}, {3} given."
+                    self.reply(e.format(parse.nick, unicode(parse.req_args), 
+                            len(parse.args))
 
-    def get_command_instance(self, command_name):
-        commands = [cls for cls in self._command_obj.__inheritors__.items()]
-        for command in commands:
-            if command.command_name == command:
-                return command
-            else:
-                continue
+    def get_hooks(self, name):
+        """Get all the hooks that can be used to call a command
+        from IRC."""
+        if not name in self.manager.resources.keys():
+            raise ValueError("{0} is not a valid command.".format(name))
+        return getattr(self.manager.resources[name], "hooks")
 
-    def get_command_hooks(self, command_name):
-        for command in self._commands.keys():
-            if command == command_name:
-                return self.get_command_instance(command).callable_hooks
-            else:
-                continue
+    def get_docs(self, name):
+        """Get the help documentation of a command."""
+        if not name in self.manager.resources.keys():
+            raise ValueError("{0} is not a valid command.".format(name))
+        return getattr(self.manager.resources[name], "help_docs")
 
-    def get_command_docs(self, command_name):
-        for command in self._commands.keys():
-            if command == command_name:
-                return self.get_command_instance(command).help_docs
-            else:
-                continue
+    def get_required_args(self, name):
+        """Gets the amount of required arguments for a command."""
+        if not name in self.manager.resources.keys():
+            raise ValueError("{0} is not a valid command.".format(name))
+        return getattr(self.manager.resources[name], "req_args")
 
     def __repr__(self):
         """Reutrn a canonical string representation of IRC."""
