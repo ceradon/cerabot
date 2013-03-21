034f14d73fb75a4d5350a405685c99cbf7f655a3
ceradon <ceradon_wiki@hotmail.com>
2013-03-21 18:19:12 -0300

    Update manager.py

--- cerabot/irc/manager.py
+++ cerabot/irc/manager.py
@@ -13,26 +13,26 @@ class CommandManager(_Manager):
         dir = path.join(path.dirname(__file__), "commands")
         self._load_directory(dir)
 
-    def _wrap_assert(self, command, parse):
+    def _wrap_assert(self, command, data):
         """Tries to assert whether *command* is being called."""
         try:
-            return command.assert_command(parse)
+            return command.assert_command(data)
         except Exception:
             e = "Error asserting command {0}"
             print e.format(command.name)
 
-    def _wrap_call(self, command, parse):
+    def _wrap_call(self, command, data):
         """Calls and command."""
         try:
-            command.call(parse)
+            command.call(data)
         except Exception:
             e = "Error calling command {0}"
             print e.format(command.name)
 
-    def call(self, hook, parse):
+    def call(self, hook, data):
         for name, command in self.resources.items():
-            if hook in command.hooks and self._wrap_assert(command, parse):
-                thread = Thread(target=self._wrap_call, args=(command, parse))
+            if hook in command.hooks and self._wrap_assert(command, data):
+                thread = Thread(target=self._wrap_call, args=(command, data))
                 start_time = strftime("%b %d %H:%M:%S")
                 thread.name = "command:{0} ({1})".format(command.name,
                         start_time)
