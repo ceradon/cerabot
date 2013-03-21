94b56e69d7615bd347cce9b413bbe445d13b0a3b
ceradon <ceradon_wiki@hotmail.com>
2013-03-21 18:33:17 -0300

    Start bot module.

---
+++ cerabot/bot.py
@@ -0,0 +1,9 @@
+import sys
+
+class Bot(object):
+    """Main bot class. Acts as the core of Cerabot. Initiates
+    instances of commands and tasks and all the things that they 
+    will need."""
+
+    def __init__(self):
+        raise NotImplementedError()
