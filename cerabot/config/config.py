5cb2c32898ad57c9fcb9bcaa9b64309ae51758f7
ceradon <ceradon_wiki@hotmail.com>
2013-03-23 01:17:49 -0300

    Update config.py

--- cerabot/config/config.py
+++ cerabot/config/config.py
@@ -1,6 +1,8 @@
 import re
 import sys
 import yaml
+import stat
+import logging
 try:
     import bcrypt
 except ImportError:
@@ -9,9 +11,11 @@ try:
     import Crypto.Cipher.Blowfish as blowfish
 except ImportError:
     blowfish = None
+from os import path, mkdir, chmod
 from hashlib import sha256
 from getpass import getpass
 from textwrap import fill
+import logging.handlers as handlers
 from .ordered import OrderedDumper
 from collections import OrderedDict
 
@@ -72,7 +76,29 @@ class Config(object):
             return self._encrypt(password)
         return password
 
+    def _encrypt(self, password):
+        if self._cipher:
+            mod = len(password) % 8
+            if mod:
+                password = password.ljust(len(password) + (8 - mod), "\x00")
+            return self._cipher.encrypt(password).encode("hex")
+        else:
+            return password
+
+    def _ask_list(self, text):
+        print fill(re.sub("\s\s+", " ", self.PROMPT + text), self.WIDTH)
+        print "[one item per line; blank line to end]:"
+        result = []
+        while True:
+            line = raw_input(self.PROMPT)
+            if line:
+                result.append(line)
+            else:
+                return result
+
     def make_new(self):
+        if self.check():
+            raise Exception("Config already exists.")
         self._print("""Welcome to the bot config helper script. This script
             is here to assist in the creation of a configuration file to make
             the process easier when creating tasks and commands. All input
@@ -192,3 +218,98 @@ class Config(object):
                 self.data["wiki"]["shutoff"] = OrderedDict([("page", page), 
                     ("disabled", content)])
         wiki()
+
+        def irc(self, name):
+            if not type(name) is tuple:
+                raise
+            if name[0] == "irc":
+                print
+                irc = self.data["irc"]["main"] = OrderedDict()
+                msg = """Hostname of the IRC server to connect to without 
+                    the leading 'irc://':"""
+                irc["host"] = self._ask(msg, "irc.freenode.net")
+                irc["port"] = self._ask("IRC port:", 6667)
+                irc["nick"] = self._ask("IRC nickname:")
+                irc["ident"] = self._ask("IRC ident", irc["nick"].lower())
+                irc["realname"] = self._ask("IRC realname:", "Cerabot")
+                if self._ask_bool("Should the bot identify to NickServ?"):
+                    ns_user = self._ask("NickServ username:", irc["nick"])
+                    ns_pass = self._ask_pass("NickServ password:")
+                    irc["nickserv_user"] = ns_user
+                    irc["nickserv_pass"] = ns_pass
+                chans = "Should any particular channels be joined by default?"
+                irc["channels"] = self._ask_list(chans)
+
+            elif name[0] == "watcher":
+                print
+                watcher = self.data["irc"]["watcher"] = OrderedDict()
+                if wmfbot:
+                    watcher["host"] = "irc.wikimedia.org"
+                    watcher["port"] = 6667
+                else:
+                    question = """Hostname of the recent changes server to 
+                        connect to with out the leading 'irc://'"""
+                    watcher["host"] = self._ask(question)
+                    watcher["port"] = self._ask("Watcher's port:", 6667)
+                nick = self._ask("Watcher's nick:", irc.get("nick"))
+                ident = self._ask("Watcher's ident:", nick.lower())
+                watcher["nick"] = nick
+                watcher["ident"] = ident
+                realname = self._ask("Watcher's real name")
+                question = "Should the bot identify to NickServ?"
+                if not wmfbot and self._ask_bool(question):
+                    ns_user = self._ask("NickServ username:", watcher["nick"])
+                    ns_pass = self._ask("NickServ password:")
+                watcher["nickserv_username"] = ns_user
+                watcher["nickserv_password"] = ns_pass
+                if wmfbot:
+                    chan = "#{0}.{1}".format(language, sitename)
+                    watcher["channels"] = [chan]
+                else:
+                    chans = "Should any particular channels be joined by default?"
+                    watcher["cahnnels"] = self._ask_list(chans)
+        if self.data["components"]["irc"]:
+            irc(("irc"))
+        elif self.data["components"]["watcher"]:
+            irc((watcher"))
+        self._print("""The settings you have entered will now be saved in a file 
+            called config.yml within the same directory as this configuration
+            helper. YAML comprises a straight-forward format that is easily
+            human-readable so any changes to your bot's configuration can easily 
+            be made via this file.""")
+        self._save()
+
+    def _save(self):
+        try:
+            open(self.config.path, "w").close()
+            chmod(self.config.path, stat.S_IRUSR|stat.S_IWUSR)
+        except IOError:
+            print "I can't seem to write to the config file:"
+            raise
+        directory = path.join(path.dirname(__file__), "config.yml")
+        with open(directory, "config.yml"), "w") as io:
+            yaml.dump(self.data, io, OrderedDumper, ident=4, allow_unicode=True,
+                default_flow_style=False)
+
+    def _load(self):
+        filename = path.join(path.dirname(__file__), "config.yml")
+        with open(filename, 'r') as fp:
+            try:
+                self._data = yaml.load(fp, OrderedLoader)
+            except yaml.YAMLError:
+                print "Error parsing config file {0}:".format(filename)
+                raise
+
+    def _handle_missing_config(self):
+        print "Config file is either missing or empty"
+        msg = "Would you like to create a config file now? [Y/n] "
+        choice = raw_input(msg)
+        if choice.lower().startswith("n"):
+            raise exceptions.NoConfigError()
+        try:
+            return Config().make_new()
+        except KeyboardInterrupt:
+            raise exceptions.NoConfigError()
+
+    def check(self):
+        return path.exists(path.join(path.dirname(__file__), "config.yml"))
