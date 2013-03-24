bc36d196bb49b8260adb27d3237d6d24bfeed67a
ceradon <ceradon_wiki@hotmail.com>
2013-03-24 12:00:48 -0300

    Update __init__.py

--- cerabot/irc/commands/__init__.py
+++ cerabot/irc/commands/__init__.py
@@ -1,21 +1,21 @@
 class Command(object):
-    req_args = 0
     name = None
     hooks = []
     help_docs = None
 
-    def __init__(self, irc):
+    def __init__(self, bot):
         """Base class for all commands."""
-        self.irc = irc
+        self._bot = bot
 
-        self.say = lambda msg, target: self.irc.say(msg, target)
-        self.action = lambda target, msg: self.irc.action(target, msg)
-        self.notice = lambda target, msg: self.irc.notice(target, msg)
-        self.join = lambda chan: self.irc.join(chan)
-        self.part = lambda chan, msg=None: self.irc.part(chan, msg)
-        self.mode = lambda t, level, msg: self.irc.mode(target, level, 
+        self.say = lambda msg, target: self._bot.irc.say(msg, target)
+        self.action = lambda target, msg: self._bot.irc.action(target, msg)
+        self.notice = lambda target, msg: self._bot.irc.notice(target, msg)
+        self.join = lambda chan: self._bot.irc.join(chan)
+        self.part = lambda chan, msg=None: self._bot.irc.part(chan, msg)
+        self.mode = lambda t, level, msg: self._bot.irc.mode(target, level, 
                                                         msg)
-        self.ping = lambda target: self.irc.ping(target)
+        self.ping = lambda target: self._bot.irc.ping(target)
+        self.reply = lambda msg, data: self._bot.irc.reply(msg, data)
         self.setup()
 
     def setup(self):
@@ -24,29 +24,20 @@ class Command(object):
         idle by default; override if necessary."""
         pass
 
-    def assert_command(self, parse):
+    def assert_command(self, data):
         """Checks if the current command should be called as a 
         response to *parse*."""
         if self.hooks:
-            return parse.is_command and parse.command_name in self.hooks
-        return parse.is_command and parse.command_name == self.name
+            return data.is_command and data.command_name in self.hooks
+        return data.is_command and data.command_name == self.name
 
-    def call(self, parser, args=None, kwargs=None):
+    def call(self, data, args=None, kwargs=None):
         """Main entry point for the command and the 
         main means by which the task does stuff. Is
         used by other parts of the module to run the
         command when requested via IRC. Should be 
         overrided always."""
         pass
-
-    def has_args(self, parse):
-        if len(parse.args) < self.req_args:
-            code = "insufficient"
-            return False, code
-        elif len(parse.args) > self.req_args:
-            code = "excess"
-            return False, code
-        return True,
 
     def __repr__(self):
         """Returns a canonical string representation of Command."""
