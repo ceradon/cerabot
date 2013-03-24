b2cc28ea2a5915879075e355e1619a2404a6ab59
ceradon <ceradon_wiki@hotmail.com>
2013-03-23 21:12:22 -0300

    Update settings.py

--- cerabot/settings.py
+++ cerabot/settings.py
@@ -5,26 +5,30 @@ class Settings(object):
     def __init__(self):
         """Initiate the `settings` dictionary"""
         self.settings = {}
-
-        #Bot settings
-        self.settings['user'] = u"Cerabot"
-        self.settings['site'] = [(u"wikipedia", u"en")]
-        self.settings['passwd'] = u""
-        self.settings['passwd_file'] = u".passwd"
-        self.settings['user_agent'] = u"Cerabot/0.1 (wikibot; {0} {1} {2};"+ \
+        self.settings["user_agent'"] = u"Cerabot/0.1 (wikibot; {0} {1} {2};"+ \
                 u"{3})".format(platform.linux_distribution()[0], platform.version(),
                 platform.linux_distribution()[1], platform.machine)
 
-        #Wikipedia settings
-        self.settings['run_base'] = u"User:Cerabot/Run/Task {task}"
-        self.settings['summary'] = u"Task {task}: {comment}. "
-        self.settings['summary'] += "([[User:Cerabot/Run/Task {task}|bot]])"
+        #Wiki settings
+        self.settings["wiki"]["user"] = u"Cerabot"
+        self.settings["wiki"]["site"] = [(u"wikipedia", u"en")]
+        self.settings["wiki"]["passwd"] = u""
+        self.settings["wiki"]["passwd_file"] = u".passwd"
+        self.settings["wiki"]["run_base"] = u"User:Cerabot/Run/Task {task}"
+        self.settings["wiki"]["summary'] = u"Task {task}: {comment}. "
+        self.settings["wiki"]["summary"] += "([[User:Cerabot/Run/Task {task}|bot]])"
         
         #IRC settings
-        self.settings['irc_nick'] = u"Cerabot"
-        self.settings['irc_passwd'] = u""
-        self.settings['irc_server'] = u"irc.freenode.net", 6667
-        self.settings['irc_name'] = u"IRC extension to Wikipedia robot Cerabot."
-        self.settings['irc_ident'] = u"cerabot"
-        self.settings['join_on_startup'] = ["##cerabot", "##ceradon"]
-        self.settings['rc_server'] = u"irc.wikimedia.org", 6667
+        self.settings["irc"]["nick"] = u"Cerabot"
+        self.settings["irc"]["passwd"] = u""
+        self.settings["irc"]["server"] = u"irc.freenode.net", 6667
+        self.settings["irc"]["realname"] = u"IRC extension to Pyhton robot Cerabot."
+        self.settings["irc"]["ident"] = u"cerabot"
+        self.settings["irc"]["channels"] = ["##cerabot", "##ceradon"]
+
+        #Watcher settings
+        self.settings["watcher"]["nick"] = u"Cerabot"
+        self.settings["watcher"]["server"] = u"irc.wikimedia.org", 6667
+        self.settings["watcher"]["realname"] = u"IRC extension to Python robot Cerabot."
+        self.settings["watcher"]["ident"] = u"cerabot"
+        self.settings["watcher"]["channels"] = ["#en.wikipedia"]
