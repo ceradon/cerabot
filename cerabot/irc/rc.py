fe88b3c9749d41abca4625f7abde1c79877f6623
ceradon <ceradon_wiki@hotmail.com>
2013-03-25 02:27:56 -0300

    Update rc.py

--- cerabot/irc/rc.py
+++ cerabot/irc/rc.py
@@ -3,19 +3,19 @@ import sys
 
 class RC(object):
     def __init__(self, data):
-        self.diff = None
-        self.user = None
-        self.flags = None
-        self.oldid = None
-        self.title = None
-        self.data = data
-        self.comment = u""
-        self.msg_type = u""
-        self.diff_size = None
+        self._diff = None
+        self._user = None
+        self._flags = None
+        self._oldid = None
+        self._title = None
+        self._data = data
+        self._comment = u""
+        self._msg_type = u""
+        self._diff_size = None
 
         #For log messages.
-        self.target = u""
-        self.log_type = None
+        self._target = u""
+        self._log_type = None
 
         self.valid_flags = {"!":"patrolled",
                             "N":"new",
@@ -32,74 +32,74 @@ class RC(object):
                         "unable to parse this line.")
 
     def _load_edit(self):
-        line = self.parser.msg
-        self.title = line[line.index("[["):line.index("]]")][2:]
-        line = line.replace("[[{0}]]".format(self.title), "")
+        line = self._data.msg
+        self._title = line[line.index("[["):line.index("]]")][2:]
+        line = line.replace("[[{0}]]".format(self._title), "")
         line = line.strip().split(" ")
         if line[0].strip("02").startswith("http://"):
             x = re.findall("(\d+)?http://en.wikipedia.org/w/index.php"+ \
                     "?diff\=(.*?)&oldid\=(.*?)", line[0])
-            self.diff, self.oldid = x[0][1:]
+            self._diff, self._oldid = x[0][1:]
         elif line[0][0] in self.valid_flags.keys():
             self.flags = line[0]
             x = re.findall("(\d+)?http://en.wikipedia.org/w/index.php"+ \
                     "?diff=(.*?)&oldid=(.*?)", line[1])
-            self.diff, self.oldid = x[0][1:]
+            self._diff, self._oldid = x[0][1:]
         x = [i for i, x in enumerate(line) if x == "*"]
-        self.user = line[x[0]:[1]]
-        self.diff_size = line[x[1]+1].strip("(").strip(")")
-        self.comment = " ".join(line[x[1]+2:])
-        self.msg_type = u"edit"
+        self._user = line[x[0]:[1]]
+        self._diff_size = line[x[1]+1].strip("(").strip(")")
+        self._comment = " ".join(line[x[1]+2:])
+        self._msg_type = u"edit"
 
     def _load_log(self):
-        line = self.parser.msg
+        line = self._data.msg
         line = line.split(" ")
-        self.log_type = line[0].strip("[[Special:Log/").strip("]]"), line[1]
-        self.target = line[3]
-        self.comment = " ".join(line[6:])
-        self.msg_type = u"log"
+        self._log_type = line[0].strip("[[Special:Log/").strip("]]"), line[1]
+        self._target = line[3]
+        self._comment = " ".join(line[6:])
+        self._msg_type = u"log"
 
     """Allow easier access to some of the message's
     attributes."""
     @property
     def title(self):
-        return self.title
+        return self._title
 
     @property
     def diff(self):
-        return self.diff
+        return self._diff
 
     @property
     def oldid(self):
-        return self.oldid
+        return self._oldid
 
     @property
     def flags(self):
-        if self.flags:
-            return self.flags
+        if self._flags:
+            return self._flags
         else:
             return None
 
     @property
     def user(self):
-        return self.user
+        return self._user
 
     @property
     def size(self):
-        return self.diff_size
+        return self._diff_size
 
     @property
     def comment(self):
-        return self.comment
+        return self._comment
 
     @property
     def target(self):
-        return self.target
+        return self._target
 
     @property
     def log_type(self):
-        return self.log_type
+        return self._log_type
 
     @property
     def msg_type(self):
-        return self.msg_type
+        return self._msg_type
