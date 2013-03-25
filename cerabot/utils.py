4a1bae084b891197d15fd4327e660af3725db6ce
ceradon <ceradon_wiki@hotmail.com>
2013-03-25 00:56:48 -0300

    Update utils.py

--- cerabot/utils.py
+++ cerabot/utils.py
@@ -1,3 +1,5 @@
+from os import path, listdir
+
 def flatten(nested):
     """Combines a bunch of lists and/or tuples into one list."""
     unified_list = []
@@ -7,3 +9,13 @@ def flatten(nested):
         else:
             unified_list.append(x)
     return unified_list
+
+def list_all_files(directory, skip):
+    files = []
+    dir = [i for i in listdir(directory) if not i.startswith(".")]
+    for name in dir:
+        if path.isdir(path.join(directory, name)):
+            files.extend(list_all_files(path.join(directory, name)))
+        else:
+            files.append(name)
+    return files
