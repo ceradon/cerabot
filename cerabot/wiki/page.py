92c4dd951244b7109e6523fd9a2c18d41a64d48d
ceradon <ceradon_wiki@hotmail.com>
2013-03-25 23:23:26 -0300

    Update page.py

--- cerabot/wiki/page.py
+++ cerabot/wiki/page.py
@@ -7,4 +7,23 @@ class Page(object):
     """Object represents a single page on the wiki.
     On initiation, loads information that could be useful."""
 
-    def __init__(self):
+    def __init__(self, site, title="", pageid=0, follow_redirects=False):
+        self.site = site
+        self._title = title
+        self._pageid = pageid
+        self._follow_redirects = follow redirects
+
+        self._exists = 0
+        self._is_redirect = False
+        self._is_talkpage = False
+        self._last_revid = None
+        self._creator = None
+        self._level = None
+        self._content = None
+
+        self._prefix = None
+        self._namespace = 0
+
+    def _load(self):
+        """Loads the attributes of this page."""
+        raise NotImplementedError()
