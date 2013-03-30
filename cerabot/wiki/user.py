ff67e3610cc0febeb093acb72abbd2eb98964cde
ceradon <ceradon_wiki@hotmail.com>
2013-03-30 18:12:39 -0300

    Create user.py

---
+++ cerabot/wiki/user.py
@@ -0,0 +1,8 @@
+import sys
+
+class User(object):
+    """Object representing a single user on the wiki."""
+
+    def __init__(self):
+        """Constructs the User object."""
+        raise NotImplementedError()
