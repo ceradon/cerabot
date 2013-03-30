074423eae1b72419cea2faa1db4b7a4e3ec8e382
ceradon <ceradon_wiki@hotmail.com>
2013-03-29 21:33:04 -0300

    Added _load_attributes.

--- cerabot/wiki/category.py
+++ cerabot/wiki/category.py
@@ -1,4 +1,5 @@
 import sys
+from cerabot import exceptions
 from .page import Page
 
 class Category(Page):
@@ -9,10 +10,36 @@ class Category(Page):
         self._members = []
         self._subcats = []
         self._files = []
+        self._count = {}
 
     def _load_attributes(self, res=None):
         """Loads attributes about our current category."""
-        raise NotImplementedError()
+        query_one = {"action":"query", "generator":"categorymembers",
+            "gcmtitle":self.title}
+        query_two = {"action":"query", "prop":"categoryinfo", "title":
+            self.title}
+        a = res if res else self.site.query(query_one)
+        for cat in a["query"]["pages"].values():
+            if cat["ns"] == 14:
+                c = Category(self.site, cat["title"])
+                c.load_attributes()
+                self._subcats.append(c)
+            elif cat["ns"] == 6:
+                # TODO: Add a File object once it's done.
+                f = Page(self.site, cat["title"])
+                f.load()
+                self._files.append(f)
+            else:
+                p = Page(self.site, cat["title"])
+                p.load()
+                self._members.append(p)
+        a = self.site.query(query_two)
+        result = a["query"]["pages"].values()[0]["categoryinfo"]
+        self._count.update({"size":result["size"],
+                            "pages":result["pages"],
+                            "files":result["files"],
+                            "subcats":result["subcats"]
+                        })
 
     @property
     def members(self):
@@ -29,6 +56,15 @@ class Category(Page):
     @property
     def categories(self):
         return self._subcats
+
+    def size(self, member_type):
+        """Gets the amount of *member_type* in our category."""
+        try:
+            count = self._count[member_type]
+        except KeyError:
+            error = "Key {0} does not exist."
+            raise exceptions.InvalidOptionError(error.format(member_type))
+        return count
 
     def __repr__(self):
         """Return a canonical string representation of Cateogry."""
