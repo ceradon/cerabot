fe524c460bb3faddddf64942092d5b10eeda7150
ceradon <ceradon_wiki@hotmail.com>
2013-04-06 01:04:29 -0300

    Added several comments, increased versatility and proficiency.

--- cerabot/wiki/tasks/templates.py
+++ cerabot/wiki/tasks/templates.py
@@ -1,6 +1,7 @@
 #!usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
+import calendar
 import mwparserfromhell
 from dateutil.parser import parse
 from datetime import datetime, timedelta
@@ -150,6 +151,18 @@ class TemplateDater(Task):
         }
         return correct_dates
 
+    def _handle_special_dates(self, summary, template):
+        """Handles dates that require special handling rules, like
+        {{as of}}, {{update after}} and {{multiple issues}}."""
+        pass
+
+    def _add_change(self, summary, name):
+        if name.lower() in summary.keys():
+            summary[template.name.lower()] += 1
+        else:
+            summary[template.name.lower()] = 1
+        return summary
+
     def handle_page(self, page):
         """Date the maintenance templates on a particular page,
         returning the updated text and the summary for the edit."""
@@ -163,24 +176,42 @@ class TemplateDater(Task):
                 new_name = self._redirects[name]
                 if new_name.lower() != name:
                     template.name = new_name
+
             if template.name.lower() in self.templates:
+                # First, some basic checks and changes if they prove 
+                # to be true.
                 if not template.has_param("date") or template.has_param("Date"):
+                    # If the parameter's name is "Date" instead of "date"
+                    # (it happens), fix that, otherwise perform a normal
+                    # dating operation.
                     if template.has_param("Date"):
                         value = template.get("Date").value
-                        template.remove("Date")
-                        template.add("date", value)
-                    template.add("date", datetime.datetime.today()
-                            .strftime('%B %Y'))
-                    if template.name.lower() in summary.keys():
-                        summary[template.name.lower()] += 1
+                        try:
+                            date = parse(value)
+                        except ValueError:
+                            value = self.month + " " + self.year
+                        template.get("Date").value = value
+                        template.get("Date").name = "date"
                     else:
-                        summary[template.name.lower()] = 1
+                        template.add("date", datetime.datetime.today()
+                                .strftime('%B %Y'))
+                    summary = self._add_change(summary, template.name)
+
+                # Now to some more complex stuff. If CURRENTMONTHNAME or
+                # CURRENTYEAR are in the date or if the month is an invalid
+                # or incorrectly formatted date, fix that.
                 else:
-                    old_date = temp.get('date').value
-                    date = temp.get('date').value.lower()
-                    month = date.split()[0]
-                    year = date.split()[1]
-                    if month in self.correct_dates.keys():
+                    old_date = template.get('date').value
+                    date = template.get('date').value.lower()
+                    try:
+                        month = date.split()[0]
+                        year = date.split()[1]
+                    except IndexError:
+                        continue
+                    if len(date.split()) > 2:
+                        month = self.month
+                        year = self.year
+                    elif month in self.correct_dates.keys():
                         month = self.correct_dates[month]
                     if 'currentmonthname' in month.lower():
                         month = self.month
@@ -189,15 +220,28 @@ class TemplateDater(Task):
                     months = map(lambda x: x.lower(), self.correct_dates.values())
                     if not month.lower() in months:
                         month = self.month
-                    new_date = month+' '+year
+                    new_date = month+" "+year
                     if old_date != new_date:
-                        temp.get('date').value = new_date
-                        if temp.name.lower() in summary.keys():
-                            summary[temp.name.lower()] += 1
-                        else:
-                            summary[temp.name.lower()] = 1
+                        template.get('date').value = new_date
+                        summary = self._add_change(summary, template.name)
                     else:
                         continue
+            else:
+                self._handle_special_dates(summary, template)
+
+            # Check if there are any dates in other parameters.
+            # Sometimes the person who placed the template on the
+            # article forgot to add the "date=" part so if this is
+            # the case, find the date and add the date template.
+            for parameter in template.params:
+                try:
+                    val = parameter.value
+                    date = parse(val)
+                except ValueError:
+                    continue
+                parameter.name = "date"
+                summary = self._add_date(summary, template.name)
+
         msg = ', '.join('{{%s}} (%s)' % (item, summary[item]) 
                 for item in summary.keys())
         return unicode(code), msg
