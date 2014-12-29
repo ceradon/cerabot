5e01e112d4c870230ea3f272b41dda70fb3ca85e
ceradon <ceradon_wiki@hotmail.com>
2014-12-29 02:14:05 -0500

    Make code more robust.

--- didyouknow.py
+++ didyouknow.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from os import path, getcwd
+import re
 import sys
 import datetime
 from pywikibot import Page, Site, Category, User
 import mwparserfromhell as parser
-
+SUBST = u"""
+==[[{0}|{1}]] nominated for DYK!==
+{2}subst:User:Cerabot/DYK|{1}{3}\n
+ —~~~~
+ """
+ 
 class DYKNotifier():
     def __init__(self, test=False):
         self.site = Site()
         self.test = test
+        self.log = None
         self.dyk_cat = "Category:Pending DYK nominations"
-        self.subst = u"""
-        ==[[{0}|{1}]] nominated for DYK!==
-        {2}subst:User:Cerabot/DYK|{1}{3}\n
-        —~~~~
-        """
-        self.summary = u" ".join((u"[[WP:DYK|Did you know]] notifier: [[{0}]]",
+        self.summary = u" ".join((u"[[WP:DYK|Did you know?]] notifier: [[{0}]]",
         u"has been nominated! ([[User:Cerabot/Run/Task 2|bot]])"))
         self.notified_comment = u" ".join((u"\n* {{BotComment}} It appears that",
-        u"[[User:{0}|{0}]] created this article. They have been notified",
-        u"of this Did you know nomination. ([{1} diff]) Did I make an",
-        u"error? [[User talk:Ceradon|Report it to my owner!]] —~~~~"))
+        u"[[User:{0}|{0}]] created this article. They have been9 notified",
+        u"of this Did you know nomination. ([{1} diff]) <small>Bot edit: Did I",
+        u"make an error? [[User talk:Ceradon|Report it to my owner!]])</small>"
+        u"—~~~~"))
         self.notified_summary = u" ".join((u"[[WP:DYK|Did you know]] notifier:",
         u"notified {0} of DYK nomination. ([[User:Cerabot/Run/Task 2|bot]])"))
         self.not_notified_details = {
@@ -36,7 +40,7 @@ class DYKNotifier():
         self.diff_add = u" ".join((u"https://en.wikipedia.org/w/index.php?",
         u"title={0}&diff={1}"))
     
-    def do_page(self, dyk, article):
+    def _do_page(self, dyk, article):
         self.check_run_page()
         dyk_creator = unicode(dyk.oldest_revision.user)
         article_creator = unicode(article.oldest_revision.user)
@@ -44,11 +48,13 @@ class DYKNotifier():
             revs_users = dyk.contributingUsers(total=5000)
             if not article_creator in revs_users:
                 dyk_text = dyk.get()
-                a = self.creator_checks(article_creator)
+                a = self._creator_checks(article_creator)
                 if not a["check_bool"]:
                     proof = a["proof"]
                     detail = self.not_notified_details[a["type"]]
                     reason = "the user " + detail
+                    if self._already_handled(dyk, "comment"):
+                        return 1
                     dyk_put_text = self.not_notified_comment.format(
                         article_creator,
                         reason,
@@ -61,41 +67,54 @@ class DYKNotifier():
                     dyk_text = dyk_text + dyk_put_text
                     dyk.put(newtext=dyk_text,
                             comment=summary,
-                            botflag=True,
-                            minor=True
+                            botflag=True
                             )
                     return True
-                talk_title = "User talk:" + article_creator
-                talk = Page(self.site, talk_title)
-                text = talk.get()
-                put_text = self.subst.format(dyk.title(), 
-                                             article.title(),
-                                             "{{",
-                                             "}}"
+                b = Page(self.site, "User talk:" + article_creator)
+                if not self._already_handled(b, "notify"):
+                    talk_title = "User talk:" + article_creator
+                    talk = Page(self.site, talk_title)
+                    text = talk.get()
+                    put_text = SUBST.format(dyk.title(), 
+                                            article.title(),
+                                            "{{",
+                                            "}}"
                                             )
-                text = text + put_text
-                summary = self.summary.format(dyk.title())
-                talk.put(newtext=text, 
-                        comment=summary,
-                        botflag=True,
-                        minor=False
+                    text = text + put_text
+                    summary = self.summary.format(article.title())
+                    talk.put(newtext=text, 
+                            comment=summary,
+                            botflag=True
+                            )
+                    diff = self.diff_add.format(dyk.title(),
+                                                dyk.latest_revision_id
+                                                )
+                    dyk_put_text = self.notified_comment(dyk_creator,
+                                                         diff
+                                                        )
+                    dyk_text = dyk_text + dyk_put_text
+                    summary = self.notified_summary.format(article_creator)
+                    dyk.put(newtext=dyk_text,
+                            comment=summary,
+                            botflag=True
                         )
-                diff = self.diff_add.format(dyk.title(),
-                                            dyk.latest_revision_id
-                                            )
-                dyk_put_text = self.notified_comment(dyk_creator,
-                                                     diff
-                                                    )
-                dyk_text = dyk_text + dyk_put_text
-                summary = self.notified_summary.format(article_creator)
-                dyk.put(newtext=dyk_text,
-                        comment=summary,
-                        botflag=True,
-                        minor=True
+                    return True
+                elif not self._already_handled(b, "comment"):
+                    diff = self.diff_add.format(dyk.title(),
+                                                dyk.latest_revision_id
+                                                )
+                    dyk_put_text = self.notified_comment(dyk_creator,
+                                                         diff
+                                                        )
+                    dyk_text = dyk_text + dyk_put_text
+                    summary = self.notified_summary.format(article_creator)
+                    dyk.put(newtext=dyk_text,
+                            comment=summary,
+                            botflag=True
                         )
         return True
 
-    def creator_checks(self, user):
+    def _creator_checks(self, user):
         user = unicode(user)
         user_object = User(self.site, user)
         return_dict = {
@@ -124,7 +143,47 @@ class DYKNotifier():
             return_dict["type"] = "inactive"
             return return_dict
         return return_dict
-    
+
+    def _collect_handled(self):
+        a = getcwd + self.log_file
+        if path.isfile(a):
+            f = open(a, "r")
+            contents = f.read()
+            if not contents:
+                return False
+            self.log = [eval(a.strip()) for a in contents.split("\n")]
+            self.collected_handled = True
+            return True
+        return False
+
+    def _already_handled(self, article, stage):
+        if stage == "notify":
+            text = article.get()
+            code = parser.parse(text)
+            for t in code.filter_templates():
+                if not t.has("bot"):
+                    continue
+                elif unicode(t.get("bot").name).lower().strip() == "cerabot":
+                    a = t.get("text").value
+                    link = a.filter_wikilinks()[0]
+                    if unicode(link.title) == unicode(article.title()):
+                        return True
+                else:
+                    return False
+        elif stage == "comment":
+            text = article.get()
+            regex = re.compile(u"\{\{botcomment\}\}\s*(?:.*)\—\[\[user:(.*)\|(.*)\]\]",
+                re.IGNORECASE)
+            result = regex.search(text)
+            if result.group(2).lower().strip() == "cerabot":
+                return True
+            else:
+                return False
+        raise ValueError("variable 'stage' did not have a recognized value.")
+
+    def _cleanup_log(self):
+        pass
+
     def generator(self):
         cat = Category(self.site, self.dyk_cat)
         dyks = [a for a in cat.articles(namespaces=[10])]
@@ -151,7 +210,8 @@ class DYKNotifier():
             return 1
         for dyk, article in self.generator():
             print "[[" + unicode(article.title()) + "]]"
-            self.do_page(dyk, article)
+            self._do_page(dyk, article)
+        self._cleanup_log()
 
 if __name__ == "__main__":
     dykbot = DYKNotifier()
