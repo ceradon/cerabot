cf572c3347da17991a1a9835a8ebc3d5a3d69365
ceradon <ceradon_wiki@hotmail.com>
2014-12-28 20:41:31 -0500

    Alpha version (0.0.1)

--- didyouknow.py
+++ didyouknow.py
@@ -1,6 +1,143 @@
 import sys
-from pywikibot import Page, Site
+import datetime
+from pywikibot import Page, Site, Category, User
 import mwparserfromhell as parser
 
 class DYKNotifier():
-    pass
+    def __init__(self):
+        self.site = Site()
+        self.dyk_cat = "Category:Pending DYK nominations"
+        self.subst = """"
+        ==[[{0}|{1}]] nominated for DYK!==
+        {2}subst:User:Cerabot/DYK|{1}{3}\n
+        —~~~~
+        """
+        self.summary = "[[WP:DYK|Did you know]] notifier: [[{0}]] has been " \
+        "nominated! ([[User:Cerabot/Run/Task 2|bot]])"
+        self.notified_comment = "\n* {{BotComment}} It appears that " \
+        "[[User:{0}|{0}]] created this article. They have been notified " \ 
+        "of this Did you know nomination. ([{1} diff]) Did I make an " \
+        "error? [[User talk:Ceradon|Report it to my owner!]] —~~~~"
+        self.notified_summary = "[[WP:DYK|Did you know]] notifier: " \
+        "notified {0} of DYK nomination. ([[User:Cerabot/Run/Task 2|bot]])"
+        self.not_notified_details = {
+            "inactive":"has not edited in 1 year or more",
+            "blocked":"has been blocked",
+            "ip":"is an IP address"
+        }
+        self.not_notified_comment = "\n* {{BotComment}} {0} was not notified" \
+        " because {1}.{2} Did I make an error? [[User talk:Ceradon|" \ 
+        "Report it to my owner!]]
+        self.not_notified_summary = "[[WP:DYK|Did you know]] notifier: {0}" \
+        " was not notified because {1}. ([[User:Cerabot/Run/Task 2|bot]])"
+        self.diff_add = "https://en.wikipedia.org/w/index.php?" \
+                        "title={0}&diff={1}"
+    
+    def do_page(self, dyk, article):
+        dyk_creator = dyk.getCreator()
+        article_creator = article.getCreator()
+        if article_creator != dyk_creator:
+            revs_users = dyk.contributingUsers(total=5000)
+            if not article_creator in revs_users:
+                dyk_text = dyk.get()
+                a = self.creator_checks(article_creator)
+                if not a["check_bool"]:
+                    proof = a["proof"]
+                    detail = self.not_notified_details[a["type"]]
+                    reason = "the user " + detail
+                    dyk_put_text = self.not_notified_comment.format(
+                        article_creator,
+                        reason,
+                        proof
+                        )
+                    summary = self.not_notified_summary.format(
+                        artice_creator,
+                        reason
+                        )
+                    dyk_text = dyk_text + dyk_put_text
+                    dyk.put(newtext=dyk_text,
+                            comment=summary,
+                            botflag=True,
+                            minor=True
+                            )
+                    return True
+                talk_title = "User talk:" + article_creator
+                talk = Page(self.site, talk_title)
+                text = talk.get()
+                put_text = self.subst.format(dyk.title(), 
+                                             article.title(),
+                                             "{{",
+                                             "}}"
+                                            )
+                text = text + put_text
+                self.check_run_page()
+                summary = self.summary.format(dyk.title())
+                talk.put(newtext=text, 
+                        comment=summary,
+                        botflag=True,
+                        minor=False
+                        )
+                diff = self.diff_add.format(dyk.title(),
+                                            dyk.latest_revision_id
+                                            )
+                dyk_put_text = self.notified_comment(dyk_creator,
+                                                     diff
+                                                    )
+                dyk_text = dyk_text + dyk_put_text
+                summary = self.notified_summary.format(article_creator)
+                dyk.put(newtext=dyk_text,
+                        comment=summary,
+                        botflag=True,
+                        minor=True
+                        )
+    return True
+
+    def creator_checks(self, user):
+        user_object = User(self.site, user)
+        return_dict = {
+            "check_bool":True,
+            "type":None
+            "proof":""
+            }
+        if user_object.isBlocked():
+            return_dict["check_bool"] = False
+            block_add = "https://en.wikipedia.org/w/index.php?" \
+            "title=Special/Log&type=block&user={0}".format(user)
+            return_dict["proof"] = " ([" + block_add + " block log])"
+            return_dict["type"] = "blocked"
+            return return_dict
+        elif user_object.isAnonymous():
+            return_dict["check_bool"] = False
+            return_dict["type"] = "ip"
+            return return_dict
+        year = datetime.datetime.today().year
+        last_edit = [a for a in user_object.contributions(total=1)][0]
+        x = year - last_edit[2].year
+        if x >= 1:
+            return_dict["check_bool"] = False
+            contribs_add = "Special:Contributions/{0}".format(user)
+            return_dict["proof"] = " ([[" + contribs_add + "|contribs]])"
+            return_dict["type"] = "inactive"
+            return return_dict
+        return return_dict
+    
+    def generator(self):
+        cat = Category(self.site, self.dyk_cat)
+        dyks = [a for a in cat.articles(namespaces=[10])]
+        for dyk in dyks:
+            title = article.name.split("/")
+            if len(title) > 3:
+                title = title[1:]
+            else:
+                title = title[1]
+            article = Page(self.site, title)
+            yield (dyk, article)
+            
+    def deploy_task(self):
+        for dyk, article in self.generator():
+            print "[[" + article + "]]"
+            self.do_page(dyk, article)
+
+if __name__ == "__main__":
+    dykbot = DYKNotifier()
+    dykbot.deploytask()
