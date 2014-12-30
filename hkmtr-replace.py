5f2ff20e16c8bd1cd8b2dbf0c48d94d7a0bff3c6
ceradon <ceradon_wiki@hotmail.com>
2014-12-30 12:46:27 -0500

    Create deploy script.

---
+++ hkmtr-replace.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+from pywikibot import Site
+from template-replacer import TemplateReplacer
+
+class HKMTRReplace():
+    def __init__(self):
+        site = Site()
+        templates = ["Template:HK-MTRL color"]
+        target = "Template:HK-MTR color"
+        test=(True, "User:Cerabot/Sandbox")
+        bot = TemplateReplacer(site, names=templates, target=target,
+            namespaces=[0], test=test, bot="Cerabot", 
+            runpage="Run/Task 2")
+        bot.deploy_task()
+        templates = ["Template:HK-MTRL line"]
+        target = "Template:HK-MTR line"
+        bot = TemplateReplacer(site, names=templates, target=target,
+            namespaces=[0], test=test, bot="Cerabot", 
+            runpage="Run/Task 2")
+        bot.deploy_task()
